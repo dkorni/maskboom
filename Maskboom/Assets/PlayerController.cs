bef80f6811339daf7d5f23cc7361428bca74a72c
Denys Korniienko <den.creativ.kor@gmail.com>
2020-09-13 01:03:45 +0300

    Scene. Controller.

---
+++ Maskboom/Assets/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private Rigidbody _rigidbody;
+
+    [SerializeField]
+    private float _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var direction = new Vector3(horizontal, 0,vertical) *_speed * Time.deltaTime;
+
+        //_characterController.Move(direction);
+        _rigidbody.MovePosition(transform.position + direction);
+        //_rigidbody.velocity =  + direction);
+
+    }
+}
